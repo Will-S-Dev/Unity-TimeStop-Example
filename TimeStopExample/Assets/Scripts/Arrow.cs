060645626b04ec0aa52fa258ae55271b72f2ea3e
Iria_Dev <52246037+Iria-Dev@users.noreply.github.com>
2019-06-26 21:53:41 +0700

    Firsto Commit
    Added the Unity Project to GitHub

---
+++ TimeStopExample/Assets/Scripts/Arrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(rb.velocity.magnitude >= 0.2f)
+        transform.rotation = Quaternion.LookRotation(rb.velocity); //Make arrow rotation follow velocity(so it looks more like an arrow and curves upwards/downwards)
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject,0.05f);
+    }
+}
